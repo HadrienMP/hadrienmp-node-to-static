b5f55702a5845859b2192386b7ace502f7c63dce
HadrienMP <public@hadrienmp.fr>
2020-01-25 14:09:34 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const express = require('express');
+const app = setupApp();
+
+app.get('/', function (req, res) {
+    res.render('index', {realisations: readRealisations()});
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`App running on http://0.0.0.0:${port}`);
+});
+
+
+function setupApp() {
+    const app = express();
+    app.set('view engine', 'pug');
+    app.use(express.static('public'));
+    return app;
+}
+
+function readRealisations() {
+    let realisationsString = fs.readFileSync('./realisations.yaml', 'utf8');
+    return yaml.safeLoad(realisationsString);
+}
---
+++ dist.sh
@@ -0,0 +1,7 @@
+rm -rf dist
+mkdir -p dist/css dist/js dist/img
+cp public/css/* dist/css
+cp public/img/* dist/img
+cp public/js/* dist/js
+cd dist || exit
+wget http://0.0.0.0:3000
