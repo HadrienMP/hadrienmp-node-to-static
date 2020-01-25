b0c060dae1298a45cb876207bf6de6c8f882e015
HadrienMP <public@hadrienmp.fr>
2020-01-25 14:39:38 +0200

    Fix encoding

--- dist.sh
+++ dist.sh
@@ -4,4 +4,5 @@ cp public/css/* dist/css
 cp public/img/* dist/img
 cp public/js/* dist/js
 cd dist || exit
-wget http://0.0.0.0:3000
+#wget http://0.0.0.0:3000
+curl http://0.0.0.0:3000 > index.html
